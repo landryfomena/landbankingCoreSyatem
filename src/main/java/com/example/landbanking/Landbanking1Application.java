e5b342bcdbcc982ddb5174d550ef17d253a089fe
landryfomena <landryfomena09@gmail.com>
2020-09-20 02:54:53 +0100

    Initial commit

---
+++ src/main/java/com/example/landbanking/Landbanking1Application.java
@@ -0,0 +1,13 @@
+package com.example.landbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Landbanking1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Landbanking1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/landbanking/Landbanking1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.landbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Landbanking1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
